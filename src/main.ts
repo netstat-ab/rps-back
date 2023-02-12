e453b54043cd0b1c6b35d8e109f9e564b8b74aa7
groot <groot@test.local>
2023-02-12 17:54:39 +0300

    configure port through .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+dotenv.config()
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
